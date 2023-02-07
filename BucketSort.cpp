6879f4d8e3f207bddf218c67224c3311a642c1a0
alexdembele <chessdembel@gmail.com>
2023-02-07 10:03:36 +0100

    a

---
+++ BucketSort.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <cstdlib>
+# include <string>
+# include <chrono>
+# include <cmath>
+# include <vector>
+# include <fstream>
+# include <mpi.h>
+
+int main()
+{
+    printf("yousk2\n");
+    return 0;
+}
